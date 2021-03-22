80d163d0bb8f3fdc50f4b5f0795e5c8aca834ccb
Seth <seth.zapata17@gmail.com>
2021-03-21 20:08:36 -0500

    new-feature added

--- EGitTest/src/HelloWorld.java
+++ EGitTest/src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("New line is being added");
-
+		System.out.println("This is new feature addition");
 	}
 
 }
